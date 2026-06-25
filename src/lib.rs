67dc5e1240e0af0ace934e4c0a74f7f061b4d8f9
AquerKing <aquerking@163.com>
2026-06-26 01:21:08 +0800

    feat: implement calculation of first and follow sets

--- src/lib.rs
+++ src/lib.rs
@@ -28,7 +28,61 @@ pub mod fc {
      * Grammar management module for handling symbols and their unique IDs.
      */
     pub mod gmr {
-        use std::hash::Hash;
+        use std::{hash::Hash, vec::Vec};
+
+        use crate::fc::gmr::ge::GenerativeExpression;
+
+        pub fn parse_generative_expressions(
+            expressions: &Vec<String>,
+            symbol_manager: &mut self::symbol::SymbolTable<char>,
+        ) -> Vec<GenerativeExpression> {
+            let mut generative_expressions = Vec::new();
+
+            for expr_str in expressions {
+                let two_parts: Vec<&str> = expr_str.split("->").collect();
+
+                if two_parts.len() != 2 {
+                    panic!("Invalid generative expression: {}", expr_str);
+                }
+
+                let trimmed_left_side = two_parts[0].trim().to_string();
+
+                if trimmed_left_side.len() != 1 {
+                    panic!(
+                        "Invalid left side of generative expression: {}",
+                        trimmed_left_side
+                    );
+                }
+
+                let left_side_id = symbol_manager
+                    .register_symbol(&trimmed_left_side.chars().next().unwrap(), false);
+
+                let timed_right_sides: Vec<String> = two_parts[1]
+                    .split("|")
+                    .map(|s| s.trim().to_string())
+                    .collect();
+
+                for right_side in timed_right_sides {
+                    let mut right_side_ids = Vec::new();
+                    let right_side_symbols: Vec<char> = right_side.chars().collect();
+
+                    for symbol in right_side_symbols {
+                        if symbol.is_ascii_uppercase() {
+                            right_side_ids.push(symbol_manager.register_symbol(&symbol, false));
+                        } else {
+                            right_side_ids.push(symbol_manager.register_symbol(&symbol, true));
+                        }
+                    }
+
+                    generative_expressions.push(GenerativeExpression {
+                        left_side: left_side_id,
+                        right_side: right_side_ids,
+                    });
+                }
+            }
+
+            generative_expressions
+        }
 
         /**
          * A trait for types that can be symbolized and printed.
@@ -41,7 +95,7 @@ pub mod fc {
         /**
          * Symbol management module for assigning unique IDs to symbols and allowing querying by ID or symbol.
          */
-        pub(super) mod symbol {
+        pub mod symbol {
             use crate::fc::gmr::Symbolizable;
             use std::{
                 collections::{HashMap, HashSet},
@@ -61,6 +115,7 @@ pub mod fc {
                 next_unused_id: SymbolId,
                 id_symbol_map: HashMap<SymbolId, T>,
                 symbol_id_map: HashMap<T, SymbolId>,
+                pub nonterminator_symbol: HashSet<SymbolId>,
             }
 
             impl<T> SymbolTable<T>
@@ -85,13 +140,13 @@ pub mod fc {
                  * Register a new symbol and return its unique ID.
                  *  If the symbol is already registered, return its existing ID.
                  */
-                pub fn register_symbol(&mut self, symbol: &T) -> SymbolId {
+                pub fn register_symbol(&mut self, symbol: &T, is_terminator: bool) -> SymbolId {
                     if let Some(id) = self.query_id_by_symbol(&symbol) {
                         return id;
                     }
 
                     let new_id = loop {
-                        if self.next_unused_id == 0 {
+                        if self.next_unused_id <= 1 {
                             panic!("No more unused IDs available.");
                         }
 
@@ -107,6 +162,10 @@ pub mod fc {
                     self.used_ids.insert(new_id);
                     self.id_symbol_map.insert(new_id, symbol.clone());
                     self.symbol_id_map.insert(symbol.clone(), new_id);
+
+                    if !is_terminator {
+                        self.nonterminator_symbol.insert(new_id);
+                    }
 
                     new_id
                 }
@@ -126,41 +185,53 @@ pub mod fc {
             }
 
             impl SymbolTable<char> {
-                fn new() -> Self {
+                pub fn new() -> Self {
                     let mut used_ids = HashSet::new();
                     used_ids.insert(0);
+                    used_ids.insert(1);
 
                     let mut id_symbol_map = HashMap::new();
                     id_symbol_map.insert(0, 'ε');
+                    id_symbol_map.insert(1, '#');
 
                     let mut symbol_id_map = HashMap::new();
                     symbol_id_map.insert('ε', 0);
+                    symbol_id_map.insert('#', 1);
+
+                    let next_unused_id = used_ids.len() as u64;
 
                     Self {
                         used_ids: used_ids,
-                        next_unused_id: 1,
+                        next_unused_id: next_unused_id,
                         id_symbol_map: id_symbol_map,
                         symbol_id_map: symbol_id_map,
+                        nonterminator_symbol: HashSet::new(),
                     }
                 }
             }
 
             impl SymbolTable<String> {
-                fn new() -> Self {
+                pub fn new() -> Self {
                     let mut used_ids = HashSet::new();
                     used_ids.insert(0);
+                    used_ids.insert(1);
 
                     let mut id_symbol_map = HashMap::new();
                     id_symbol_map.insert(0, String::from("ε"));
+                    id_symbol_map.insert(1, String::from("#"));
 
                     let mut symbol_id_map = HashMap::new();
                     symbol_id_map.insert(String::from("ε"), 0);
+                    symbol_id_map.insert(String::from("#"), 1);
+
+                    let next_unused_id = used_ids.len() as u64;
 
                     Self {
                         used_ids: used_ids,
-                        next_unused_id: 1,
+                        next_unused_id: next_unused_id,
                         id_symbol_map: id_symbol_map,
                         symbol_id_map: symbol_id_map,
+                        nonterminator_symbol: HashSet::new(),
                     }
                 }
             }
@@ -176,6 +247,7 @@ pub mod fc {
 
             pub struct GenerativeExpressionManager {}
 
+            #[derive(Clone)]
             pub struct GenerativeExpression {
                 pub left_side: SymbolId,
                 pub right_side: Vec<SymbolId>,
@@ -188,6 +260,250 @@ pub mod fc {
         {
             pub symbol_manager: symbol::SymbolTable<T>,
             pub expressions: Vec<ge::GenerativeExpression>,
+        }
+
+        impl<T> GrammarContext<T>
+        where
+            T: Symbolizable + Eq + PartialEq + PartialOrd + Ord + Clone + Hash,
+        {
+            pub fn is_nonterminator(&self, symbol_id: symbol::SymbolId) -> bool {
+                self.symbol_manager
+                    .nonterminator_symbol
+                    .contains(&symbol_id)
+            }
+        }
+    }
+
+    pub mod calc {
+        use std::{
+            collections::{HashMap, HashSet},
+            io::ErrorKind::ConnectionAborted,
+        };
+
+        use crate::fc::gmr::symbol::SymbolId;
+
+        pub fn calculate_first_follow_sets(
+            context: &super::gmr::GrammarContext<char>,
+        ) -> (HashMap<char, HashSet<char>>, HashMap<char, HashSet<char>>) {
+            let mut first_id_sets: HashMap<u64, HashSet<u64>> = HashMap::new();
+            let mut follow_id_sets: HashMap<u64, HashSet<u64>> = HashMap::new();
+
+            // let nonterminator_ids = context
+            //     .symbol_manager
+            //     .nonterminator_symbol
+            //     .iter()
+            //     .filter(|&id| context.is_nonterminator(*id))
+            //     .map(|&id| id)
+            //     .collect::<Vec<SymbolId>>();
+
+            // Calculate first sets
+            loop {
+                let mut changed = false;
+
+                for expr in context.expressions.iter() {
+                    let left_side_id = expr.left_side;
+                    let size_backup = first_id_sets
+                        .get(&left_side_id)
+                        .map_or(0, |v: &HashSet<u64>| v.len());
+
+                    for &symbol_id in expr.right_side.iter() {
+                        // If the symbol is the epsilon symbol (ID 0), add it to the first set and break
+                        if symbol_id == 0 {
+                            first_id_sets
+                                .entry(left_side_id)
+                                .or_insert_with(|| HashSet::new())
+                                .insert(0);
+                            continue;
+                        }
+
+                        // If the symbol is a terminator, add it to its own first set and break
+                        if !context.is_nonterminator(symbol_id) {
+                            first_id_sets
+                                .entry(left_side_id)
+                                .or_insert_with(|| HashSet::new())
+                                .insert(symbol_id);
+
+                            if first_id_sets
+                                .get(&left_side_id)
+                                .map_or(0, |v: &HashSet<u64>| v.len())
+                                > size_backup
+                            {
+                                changed = true;
+                            }
+                        } else {
+                            // If the symbol is a non-terminator, add it to its own first set and continue
+                            let mut this_symbol_first_set = first_id_sets
+                                .entry(symbol_id)
+                                .or_insert_with(HashSet::new)
+                                .clone();
+
+                            let should_continue;
+
+                            if this_symbol_first_set.contains(&0) {
+                                this_symbol_first_set.remove(&0);
+                                should_continue = true;
+                            } else {
+                                should_continue = false;
+                            }
+
+                            first_id_sets
+                                .entry(left_side_id)
+                                .or_insert_with(HashSet::new)
+                                .extend(this_symbol_first_set);
+
+                            if should_continue {
+                                continue;
+                            }
+                        }
+
+                        break;
+                    }
+
+                    if first_id_sets
+                        .get(&left_side_id)
+                        .map_or(0, |v: &HashSet<u64>| v.len())
+                        > size_backup
+                    {
+                        changed = true;
+                    }
+
+                    println!(
+                        "First set for {}: {:?}",
+                        context
+                            .symbol_manager
+                            .get_symbol_by_id(expr.left_side)
+                            .unwrap(),
+                        first_id_sets
+                            .get(&expr.left_side)
+                            .unwrap()
+                            .iter()
+                            .map(|&id| context.symbol_manager.get_symbol_by_id(id))
+                            .collect::<Vec<_>>()
+                    );
+                }
+
+                if !changed {
+                    break;
+                }
+            }
+
+            // Calculate follow sets
+            loop {
+                let mut changed = false;
+
+                follow_id_sets
+                    .entry(context.expressions[0].left_side)
+                    .or_insert_with(HashSet::new)
+                    .insert(1);
+
+                // Calculate follow sets
+                for expr in context.expressions.iter() {
+                    let left_side_id = expr.left_side;
+
+                    for (i, &symbol_id) in expr.right_side.iter().enumerate() {
+                        // If the symbol is a terminator, skip it
+                        if !context.is_nonterminator(symbol_id) {
+                            continue;
+                        }
+
+                        let size_backup = follow_id_sets
+                            .get(&symbol_id)
+                            .map_or(0, |v: &HashSet<u64>| v.len());
+
+                        // If the symbol is the last one in the right side, add the follow set of the left side to its follow set
+                        if i == expr.right_side.len() - 1 {
+                            let left_side_follow_set = follow_id_sets
+                                .entry(left_side_id)
+                                .or_insert_with(HashSet::new)
+                                .clone();
+
+                            follow_id_sets
+                                .entry(symbol_id)
+                                .or_insert_with(HashSet::new)
+                                .extend(left_side_follow_set);
+                        } else {
+                            // If the symbol is not the last one, add the first set of the next symbol to its follow set
+                            let next_symbol_id = expr.right_side[i + 1];
+
+                            // If the next symbol is terminator, add it to this symbol's follow set.
+                            if !context.is_nonterminator(next_symbol_id) {
+                                follow_id_sets
+                                    .entry(symbol_id)
+                                    .or_insert_with(HashSet::new)
+                                    .insert(next_symbol_id);
+                            } else {
+                                let mut next_symbol_id = next_symbol_id;
+
+                                while context.is_nonterminator(next_symbol_id) {
+                                    let next_symbol_first_set = first_id_sets
+                                        .entry(next_symbol_id)
+                                        .or_insert_with(HashSet::new)
+                                        .clone();
+
+                                    follow_id_sets
+                                        .entry(symbol_id)
+                                        .or_insert_with(HashSet::new)
+                                        .extend(
+                                            next_symbol_first_set.iter().filter(|&&id| id != 0),
+                                        );
+
+                                    if next_symbol_first_set.contains(&0) {
+                                        next_symbol_id += 1;
+                                        continue;
+                                    } else {
+                                        break;
+                                    }
+                                }
+
+                                if !context.is_nonterminator(next_symbol_id) {
+                                    follow_id_sets
+                                        .entry(symbol_id)
+                                        .or_insert_with(HashSet::new)
+                                        .insert(next_symbol_id);
+                                }
+                            }
+                        }
+
+                        if follow_id_sets
+                            .get(&symbol_id)
+                            .map_or(0, |v: &HashSet<u64>| v.len())
+                            > size_backup
+                        {
+                            changed = true;
+                        }
+                    }
+                }
+
+                if !changed {
+                    break;
+                }
+            }
+
+            let first_sets: HashMap<char, HashSet<char>> = first_id_sets
+                .iter()
+                .map(|(&id, set)| {
+                    let symbol = context.symbol_manager.get_symbol_by_id(id).unwrap();
+                    let symbol_set: HashSet<char> = set
+                        .iter()
+                        .filter_map(|&id| context.symbol_manager.get_symbol_by_id(id))
+                        .collect();
+                    (symbol, symbol_set)
+                })
+                .collect();
+
+            let follow_sets: HashMap<char, HashSet<char>> = follow_id_sets
+                .iter()
+                .map(|(&id, set)| {
+                    let symbol = context.symbol_manager.get_symbol_by_id(id).unwrap();
+                    let symbol_set: HashSet<char> = set
+                        .iter()
+                        .filter_map(|&id| context.symbol_manager.get_symbol_by_id(id))
+                        .collect();
+                    (symbol, symbol_set)
+                })
+                .collect();
+
+            (first_sets, follow_sets)
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::fs::File;
+
+use cdpc::fc::{self, calc::calculate_first_follow_sets, gmr::symbol::SymbolTable};
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    grammar: String,
+
+    #[arg(default_value = "output.txt")]
+    output: Option<String>,
+}
 
 fn main() {
+    let args = Args::parse();
+
+    if args.grammar.is_empty() {
+        eprintln!("Error: Grammar file path is required.");
+        std::process::exit(1);
+    }
+
+    let mut symbol_manager = fc::gmr::symbol::SymbolTable::<char>::new();
+
+    let grammar_content = std::fs::read_to_string(args.grammar.to_string()).unwrap();
+    let ge_strs = fc::sp::get_ge_from_str(&grammar_content.to_string());
+    let ges = fc::gmr::parse_generative_expressions(&ge_strs, &mut symbol_manager);
+
+    let context = fc::gmr::GrammarContext {
+        symbol_manager: symbol_manager,
+        expressions: ges,
+    };
+
+    let (first_sets, follow_sets) = calculate_first_follow_sets(&context);
+
+    let contents = format!(
+        "First Sets:\n{:?}\n\nFollow Sets:\n{:?}",
+        first_sets, follow_sets
+    );
+    std::fs::write(args.output.unwrap_or_else(|| "output.txt".into()), contents).unwrap();
 }
